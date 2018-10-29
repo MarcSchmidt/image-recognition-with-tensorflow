8cf0dddc5b3f4f114906cf3b1b34c69d02654746
Jann Deterling <jann.deterling@smail.th-koeln.de>
2018-10-29 13:55:19 +0100

    it wokrs somehow

--- cnn.py
+++ cnn.py
@@ -11,6 +11,8 @@ import tensorflow as tf
 from tensorflow import keras as ks
 import numpy as np
 
+(train_img, train_label), (test_img, test_label) = tf.keras.datasets.cifar10.load_data()
+
 # ---------- Shape the CNN ----------
 # Initialising the CNN as sequential model
 classifier = ks.models.Sequential()
@@ -22,22 +24,13 @@ classifier = ks.models.Sequential()
 # (3, 3)        - shape of each Filter
 # (64, 64, 3)   - Shape of Input (Rows, Columns, Channels)
 # 'relu'        - Activation function
-classifier.add(ks.layers.Conv2D(32, (3, 3), input_shape=(64, 64, 3), activation='relu'))
+classifier.add(ks.layers.Conv2D(32, (3, 3), input_shape=(32, 32, 3), activation='relu', data_format='channels_last') )
 classifier.add(ks.layers.Conv2D(32, (3, 3), activation='relu'))
 
 # Pooling Layer
 # Reduce the size of the input data by 75%
 # (2, 2) - Map 2x2 inputs to one output
 classifier.add(ks.layers.MaxPooling2D(pool_size=(2, 2)))
-# Dropout Layer
-# Remove randomly some nodes to add noise
-classifier.add(ks.layers.Dropout(0.25))
-
-# Convolution Layer
-classifier.add(ks.layers.Conv2D(64, (3, 3), activation='relu'))
-classifier.add(ks.layers.Conv2D(64, (3, 3), activation='relu'))
-classifier.add(ks.layers.MaxPooling2D(pool_size=(2, 2)))
-classifier.add(ks.layers.Dropout(0.25))
 
 # Flattening Layer
 # Maps a X by X Matrix to one Vector
@@ -98,16 +91,6 @@ def test_input_fn():
     return tf.data.Dataset.from_generator(test_set, output_types=(tf.int64, tf.int64))
 
 
-def input_fn():
-    x = np.random.random((1024, 10))
-    y = np.random.randint(2, size=(1024, 1))
-    x = tf.cast(x, tf.float32)
-    dataset = tf.data.Dataset.from_tensor_slices((x, y))
-    dataset = dataset.repeat(100)
-    dataset = dataset.batch(32)
-    return dataset
-
-
 # ---------- Start Training ----------
 # Configure Tensorboard for Keras with defined log name
 tensorboard = ks.callbacks.TensorBoard(log_dir="logs/{}".format(time()),
@@ -128,15 +111,25 @@ tensorboard = ks.callbacks.TensorBoard(log_dir="logs/{}".format(time()),
 #                          validation_steps=312,
 #                          callbacks=[tensorboard])
 
-(train_img, train_label), (test_img, test_label) = tf.keras.datasets.cifar10.load_data()
+
+# Convert class vectors to binary class matrices.
+y_train = ks.utils.to_categorical(train_label, 10)
+y_test = ks.utils.to_categorical(test_label, 10)
+
+train_img = train_img.astype('float32')
+test_img = test_img.astype('float32')
+
+train_img /= 255
+test_img /= 255
 
 
 def input_training():
     # x = tf.cast(input_train_data_img, tf.float32)
     # training_set = tf.data.Dataset.from_tensor_slices((x, input_train_data_label))
-    training_set = tf.data.Dataset.from_tensor_slices((train_img, train_label))
-    training_set.batch(32)
-
+    training_set = tf.data.Dataset.from_tensor_slices((train_img, y_train))
+    training_set = training_set.take(100).batch(32)
+    print(train_img.shape)
+    print(training_set.output_shapes)
     return training_set
 
 
@@ -144,8 +137,10 @@ def input_validation():
     # input_eval_data_img, input_eval_data_label = input_train_data_img, input_train_data_label
     # x = tf.cast(input_eval_data_img, tf.float32)
     # validation_set = tf.data.Dataset.from_tensor_slices((x, input_eval_data_label))
-    validation_set = tf.data.Dataset.from_tensor_slices((test_img, test_label))
-    validation_set.batch(32)
+    validation_set = tf.data.Dataset.from_tensor_slices((test_img, y_test))
+    validation_set = validation_set.take(100).batch(32)
+    print(test_img.shape)
+    print(validation_set.output_shapes)
     return validation_set
 
 
@@ -163,4 +158,5 @@ def model_main():
 
 
 # Call the model_main function defined above.
+os.environ["TF_CONFIG"] = '{"cluster": {"chief": ["localhost:2223"],"worker": ["localhost:2222"]},"task": {"type": "chief", "index": 0}}'
 model_main()
