3c865d6151f521522ad46eb7ec0a23b9a6531654
Jann Deterling <jann.deterling@smail.th-koeln.de>
2019-01-09 10:13:09 +0100

    add installer

---
+++ kubernetes/kube_installer.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+apt-get update && apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
+apt-get install -y kubelet kubeadm kubectl docker.io
+apt-mark hold kubelet kubeadm kubectl
+systemctl enable docker.service
+swapoff -a
+kubeadm config images pull
+
+kubeadm join <<ip-addr>> --token <<token>> --discovery-token-ca-cert-hash <<hash>>
---
+++ kubernetes/kube_master_installer.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+apt-get update && apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
+apt-get install -y kubelet kubeadm kubectl docker.io
+apt-mark hold kubelet kubeadm kubectl
+systemctl enable docker.service
+swapoff -a
+kubeadm config images pull
+kubeadm init
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
