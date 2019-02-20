e240025595e4aed873d4aeca3d11aae8044a8fac
Jann Deterling <jann.deterling@smail.th-koeln.de>
2019-02-20 11:07:50 +0100

    update deployment

--- kubernetes/copy_cred.sh
+++ kubernetes/copy_cred.sh
@@ -1,4 +1,4 @@
 # !bin/bash
 
 nodes=$(kubectl get nodes -o jsonpath='{range.items[*].metadata}{.name} {end}')
-for n in $nodes; do scp ~/.docker/config.json root@$n:/var/lib/kubelet/config.json; done
+for n in $nodes; do scp ~/.docker/config.json kube@$n:/var/lib/kubelet/config.json; done
