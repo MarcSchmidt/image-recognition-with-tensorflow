0e252fb333f3c0a02db3c31a77b7add80f0358f5
Jann Deterling <jann.deterling@smail.th-koeln.de>
2019-02-20 12:56:47 +0100

    update deployment

--- kubernetes/run_cluster.sh
+++ kubernetes/run_cluster.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-docker build -t docker.nexus.archi-lab.io/archilab/tensorflow-img-rec ../
-docker push docker.nexus.archi-lab.io/archilab/tensorflow-img-rec
+docker build -t tensorflow-img-rec ../
 kubectl -n tensorflow delete po,svc,statefulsets,deployments  --all
 kubectl apply -f ./deployment.yaml
