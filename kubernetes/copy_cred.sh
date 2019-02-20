ea010d5115f8c04432e1c70b8cbfc4b43cb3032e
Jann Deterling <jann.deterling@smail.th-koeln.de>
2019-02-20 11:18:57 +0100

    update deployment

--- kubernetes/copy_cred.sh
+++
@@ -1,4 +0,0 @@
-# !bin/bash
-
-nodes=$(kubectl get nodes -o jsonpath='{range .items[*].status.addresses[?(@.type=="ExternalIP")]}{.address} {end}')
-for n in $nodes; do sudo scp ~/.docker/config.json kube@$n:/var/lib/kubelet/config.json; done
---
+++ kubernetes/pull-img.sh
@@ -0,0 +1,4 @@
+# !bin/bash
+
+docker login https://docker.nexus.archi-lab.io
+docker pull docker.nexus.archi-lab.io/archilab/tensorflow-img-rec
